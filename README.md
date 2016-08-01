5c0edb590a01e20370959fd928021bd1e829d617
toby ealden <toby.ealden@gmail.com>
2016-08-01 12:47:06 +0100

    reset version for first publish

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # nqm-api-tdx
 nquiringminds Trusted Data Exchange command and query API interface for nodejs clients
+
+## install
+npm install nqm-api-tdx
+
+## usage
+
+```
+var TDXApi = require("nqm-api-tdx");
+
+var config = {
+  baseCommandURL: "https://cmd.nqminds.com",
+  baseQueryURL: "https://q.nqminds.com"  
+};
+
+var nqmindsTDX = new TDXApi(config);
+
+nqmindsTDX.authenticate("myTokenID","myTokenSecret", function(err, accessToken) {
+
+});
+```
---
+++ test/basic.js
@@ -0,0 +1,50 @@
+var chai = require("chai");
+var expect = chai.expect;
+
+var APIModule = require("../lib/api.js");
+var testConfig = {
+  baseCommandURL: "https://cmd.nqminds.com",
+  baseQueryURL: "https://q.nqminds.com"
+};
+var api = new APIModule(testConfig);
+
+describe("authenticate", function() {
+  it("fails to authenticate invalid credentials", function(done) {
+    api.authenticate("foo","bar",function(err, token) {
+      expect(token).to.not.exist;
+      expect(err).to.exist;
+      done();
+    });
+  });
+  it("authenticates valid credentials", function(done) {
+    api.authenticate("Skgsbgond","password",function(err, token) {
+      expect(token).to.exist;
+      expect(err).to.not.exist;
+      done();
+    });
+  });
+});
+
+describe("query", function() {
+  it("queries dataset data", function(done) {
+    api.query("datasets/NJgMR6EPmg/data", null, null, null, function(err, data) {
+      expect(err).to.not.exist;
+      expect(data).to.exist;
+      done();
+    });
+  });
+});
+
+describe("commands", function() {
+  it("creates dataset", function(done) {
+    var createOptions = {
+      name: "test dataset",
+      parentId: "H1xqLgo2_",
+      basedOnSchema: "dataset"
+    };
+    api.createDataset(createOptions, function(err, id) {
+      expect(err).to.not.exist;
+      done();
+    });
+  })
+});
