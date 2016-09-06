8597d8a138ddf205c87078d01d22d41017c5a7d9
toby ealden <toby.ealden@gmail.com>
2016-09-06 22:58:15 +0100

    fix error handling - superagent does something weird with the response?

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 module.exports = (function() {
   "use strict";
 
+  // To enable debug.
+  if (!process.env.DEBUG) {
+    process.env.DEBUG = "nqm-*";
+  }
+  // Default output to STDOUT rather than STDERR.
+  process.env.DEBUG_FD = 1;
+
   var API = require("./lib/api.js");
   return API;
 }())
