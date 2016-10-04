bff24c555ddd3f696a48d608fdc0b12496578c44
toby ealden <toby.ealden@gmail.com>
2016-10-05 00:01:22 +0100

    don't force DEBUG settings

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
 module.exports = (function() {
   "use strict";
 
-  // To enable debug.
-  if (!process.env.DEBUG) {
-    process.env.DEBUG = "nqm-*";
-  }
-  // Default output to STDOUT rather than STDERR.
-  process.env.DEBUG_FD = 1;
-
   var API = require("./lib/api.js");
   return API;
 }())
