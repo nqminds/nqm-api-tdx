8ae25b099c2cf90f3037d1e0f8d7bc43a14315f1
Toby Ealden <toby.ealden@gmail.com>
2021-04-27 17:21:19 +0100

    support auth server at host root

--- src/helpers.js
+++ src/helpers.js
@@ -1,17 +1,18 @@
 import crossFetch from "cross-fetch";
 import debug from "debug";
+import TDXApiError from "./api-tdx-error.js";
 
 // Default 'debug' module output to STDOUT rather than STDERR.
 debug.log = console.log.bind(console); // eslint-disable-line no-console
 
-const fetch = (typeof window !== "undefined" && window.fetch) ? window.fetch : crossFetch;
+const fetch = typeof window !== "undefined" && window.fetch ? window.fetch : crossFetch;
 const FetchRequest = fetch.Request || Request;
 const FetchHeaders = fetch.Headers || Headers;
 const pollingRetries = 15;
 const pollingInterval = 1000;
 const waitInfinitely = -1;
 
-const fetchWithDeadline = function(request) {
+const fetchWithDeadline = function (request) {
   const log = debug("nqm-api-tdx:fetchWithDeadline");
 
   //
@@ -24,15 +25,12 @@ const fetchWithDeadline = function(request) {
 
     if (this.config.networkTimeout) {
       // Reject the promise if the timeout expires.
-      deadline = setTimeout(
-        () => {
-          log("deadline expired after %d ms", this.config.networkTimeout);
-          deadline = 0;
-          rejected = true;
-          reject(new Error(`deadline expired after ${this.config.networkTimeout} ms`));
-        },
-        this.config.networkTimeout
-      );
+      deadline = setTimeout(() => {
+        log("deadline expired after %d ms", this.config.networkTimeout);
+        deadline = 0;
+        rejected = true;
+        reject(new Error(`deadline expired after ${this.config.networkTimeout} ms`));
+      }, this.config.networkTimeout);
     } else {
       // Never timeout.
       deadline = 0;
@@ -63,101 +61,6 @@ const fetchWithDeadline = function(request) {
   });
 };
 
-// Babel 7 finally supports `extends Error` https://github.com/babel/babel/pull/7020
-// it only doesn't work in IE 10
-
-/**
- * The TDX api supplies detailed error information depending on the context of the call.
- * In some instances, e.g. attempting to retrieve a resource that does not exist, the
- * error will be a simple `NotFound` string message. In other cases, e.g. attempting
- * to update 100 documents in a single call, the error will supply details for each
- * document update that failed, such as the primary key of the document and the reason
- * for the failure.
- * @example
- * <caption>`failure` for simple query error</caption>
- * const failure = {
- *   code: "NotFound",
- *   message: "resource not found: KDiEI3k_"
- * };
- * @example
- * <caption>`failure` for complex data update error</caption>
- * const failure = {
- *  code: "BadRequestError",
- *  message: [
- *    {
- *      key: {id: "foo"},
- *      error: {
- *        message: "document not found matching key 'foo'"
- *      }
- *    },
- *    {
- *      key: {id: "bar"},
- *      error: {
- *        message: "'hello' is not a valid enum value",
- *        name: "ValidatorError",
- *        kind: "enum"
- *        path: "value"
- *      }
- *    }
- *  ]
- * };
- */
-class TDXApiError extends Error {
-  constructor(code, failure, source, stack) {
-    // Build a string summary for legacy or non-json clients.
-    const stringVersion = JSON.stringify({
-      from: source,
-      failure: JSON.stringify(failure),
-      code,
-    });
-
-    super();
-    /**
-     * `"TDXApiError"`, indicating the error originated from this library.
-     * @type {string}
-     */
-    this.name = "TDXApiError";
-    /**
-     * The HTTP response status code, e.g. 401.
-     * @type {number}
-     */
-    this.code = code;
-    /**
-     * A string-encoded form of the error, essentially a JSON stringified
-     * copy of the entire error object.
-     * @deprecated This is included for legacy reasons and may be removed in a future release.
-     * @type {string}
-     */
-    this.message = stringVersion;
-    /**
-     * Usually the name of the API call that originated the error, e.g. updateData
-     * @type {string}
-     */
-    this.from = source;
-
-    /**
-     * @typedef {object} FailureObject - Error information as received from the TDX
-     * @property {string} code - the TDX short error code, e.g. NotFound, PermissionDenied etc.
-     * @property {string|array} message - details of the failure. For simple cases this will be a string,
-     * e.g. `resource not found: KDiEI3k_`. In other instances this will be an array of objects describing each error.
-     * See the example below showing a failed attempt to update 2 documents.
-     * One of the errors is a simple document not found and the other is a validation error giving details of the
-     * exact path in the document that failed validation.
-     */
-
-    /**
-     * An object containing the error information as received from the TDX
-     * @type {FailureObject}
-     */
-    this.failure = failure;
-    /**
-     * The stack trace
-     * @type {string}
-     */
-    this.stack = stack || (new Error(this.message)).stack;
-  }
-}
-
 /**
  * Formats a TDXApiError object.
  * @param  {string} source - The source of the error, usually a function name.
@@ -165,23 +68,18 @@ class TDXApiError extends Error {
  * @param  {string} code - The error code, usually the response status code, e.g. 422, 401 etc.
  * @returns {TDXApiError}
  */
-const handleError = function(code, failure, source) {
-  return new TDXApiError(
-    typeof code === "undefined" ? "n/a" : code,
-    failure,
-    source,
-    (new Error()).stack
-  );
+const handleError = function (code, failure, source) {
+  return new TDXApiError(typeof code === "undefined" ? "n/a" : code, failure, source, new Error().stack);
 };
 
-const buildAuthenticateRequest = function(credentials, ip, ttl) {
+const buildAuthenticateRequest = function (credentials, ip, ttl) {
   // We can get a token from any of the TDX services - use the first one we find to build a fetch Request.
   const uri = `${this.config.tdxServer || this.config.commandServer || this.config.queryServer}/token`;
   return new FetchRequest(uri, {
     method: "POST",
     mode: "cors",
     headers: new FetchHeaders({
-      "Authorization": `Basic ${credentials}`,
+      Authorization: `Basic ${credentials}`,
       "Content-Type": "application/json",
     }),
     body: JSON.stringify({grant_type: "client_credentials", ip, ttl: ttl || this.config.accessTokenTTL || 3600}),
@@ -195,14 +93,14 @@ const buildAuthenticateRequest = function(credentials, ip, ttl) {
  * @param  {string} [contentType=application/json] - the content type
  * @param  {boolean} [noSync=false] - send command asynchronously
  */
-const buildCommandRequest = function(command, data, contentType, async) {
+const buildCommandRequest = function (command, data, contentType, async) {
   const commandMode = async ? "command" : "commandSync";
   contentType = contentType || "application/json";
   return new FetchRequest(`${this.config.commandServer}/${commandMode}/${command}`, {
     method: "POST",
     mode: "cors",
     headers: new FetchHeaders({
-      "Authorization": `Bearer ${this.accessToken}`,
+      Authorization: `Bearer ${this.accessToken}`,
       "Content-Type": contentType,
     }),
     body: JSON.stringify(data),
@@ -215,7 +113,7 @@ const buildCommandRequest = function(command, data, contentType, async) {
  * @param  {object} data - the command payload
  * @param  {string} [contentType=application/json] - the content type
  */
-const buildDatabotHostRequest = function(command, data) {
+const buildDatabotHostRequest = function (command, data) {
   if (!this.config.databotServer) {
     throw new Error("databotServer URL not defined in API config");
   }
@@ -224,14 +122,14 @@ const buildDatabotHostRequest = function(command, data) {
     method: "POST",
     mode: "cors",
     headers: new FetchHeaders({
-      "Authorization": `Bearer ${this.accessToken}`,
+      Authorization: `Bearer ${this.accessToken}`,
       "Content-Type": "application/json",
     }),
     body: JSON.stringify(data),
   });
 };
 
-const buildFileUploadRequest = function(resourceId, compressed, base64Encoded, file) {
+const buildFileUploadRequest = function (resourceId, compressed, base64Encoded, file) {
   let endPoint;
   if (compressed) {
     endPoint = "compressedUpload";
@@ -245,7 +143,7 @@ const buildFileUploadRequest = function(resourceId, compressed, base64Encoded, f
     method: "POST",
     mode: "cors",
     headers: new FetchHeaders({
-      "Authorization": `Bearer ${this.accessToken}`,
+      Authorization: `Bearer ${this.accessToken}`,
       "Content-Length": file.size,
       "Content-Disposition": `attachment; filename="${file.name}"`,
     }),
@@ -261,7 +159,7 @@ const buildFileUploadRequest = function(resourceId, compressed, base64Encoded, f
  * temperature: 1}
  * @param  {object} [options] - query options, e.g. {limit: 10, sort: {timestamp: -1}}
  */
-const buildQueryRequest = function(endpoint, filter, projection, options) {
+const buildQueryRequest = function (endpoint, filter, projection, options) {
   filter = filter ? encodeURIComponent(JSON.stringify(filter)) : "";
   projection = projection ? encodeURIComponent(JSON.stringify(projection)) : "";
   options = options ? encodeURIComponent(JSON.stringify(options)) : "";
@@ -277,11 +175,11 @@ const buildQueryRequest = function(endpoint, filter, projection, options) {
     method: "GET",
     mode: "cors",
     headers: new FetchHeaders({
-      "Authorization": `Bearer ${this.accessToken}`,
+      Authorization: `Bearer ${this.accessToken}`,
       "Content-Type": "application/json",
       "Cache-Control": "no-cache",
-      "Pragma": "no-cache",
-      "Expires": "0",
+      Pragma: "no-cache",
+      Expires: "0",
     }),
   });
 };
@@ -290,7 +188,7 @@ const buildQueryRequest = function(endpoint, filter, projection, options) {
  * Builds a Request object for the given databot instance query bound to the TDX databot server.
  * @param  {string} endpoint - the databot query endpoint, e.g. "status/jDduieG7"
  */
-const buildDatabotInstanceRequest = function(endpoint) {
+const buildDatabotInstanceRequest = function (endpoint) {
   if (!this.config.databotServer) {
     throw new Error("databotServer URL not defined in API config");
   }
@@ -299,16 +197,16 @@ const buildDatabotInstanceRequest = function(endpoint) {
     method: "GET",
     mode: "cors",
     headers: new FetchHeaders({
-      "Authorization": `Bearer ${this.accessToken}`,
+      Authorization: `Bearer ${this.accessToken}`,
       "Content-Type": "application/json",
       "Cache-Control": "no-cache",
-      "Pragma": "no-cache",
-      "Expires": "0",
+      Pragma: "no-cache",
+      Expires: "0",
     }),
   });
 };
 
-const checkResponse = function(source, doNotThrow, response) {
+const checkResponse = function (source, doNotThrow, response) {
   const log = debug("nqm-api-tdx:checkResponse");
 
   // If doNotThrow is omitted default to the config value (which defaults to `false`, i.e. errors will be thrown).
@@ -317,63 +215,62 @@ const checkResponse = function(source, doNotThrow, response) {
     doNotThrow = !!this.config.doNotThrow;
   }
 
-  return response.text()
-    .then((text) => {
-      let jsonResponse;
+  return response.text().then((text) => {
+    let jsonResponse;
 
-      try {
-        // Attempt to parse JSON, we could check the content-type header first?
-        jsonResponse = JSON.parse(text);
-      } catch (parseError) {
-        log("failed to parse json => assuming non-JSON content type");
-      }
+    try {
+      // Attempt to parse JSON, we could check the content-type header first?
+      jsonResponse = JSON.parse(text);
+    } catch (parseError) {
+      log("failed to parse json => assuming non-JSON content type");
+    }
 
-      if (response.ok) {
-        if (jsonResponse) {
-          // Successfully parsed JSON content.
-          // Check for data write errors. These differ from straight-forward invalid argument or validation
-          // failures. For example, a call to `updateData` might include requests to update 10 documents.
-          // If all 10 documents pass validation, the TDX will go ahead and attempt to write the data and
-          // will continue to apply updates even after one of them fails, i.e. the first 4 updates succeed,
-          // the fifth fails and the rest succeed. In this case `tdxResponse` will contain a `result` object with
-          // details of the failures in an `error` array property and the successes in an `commit` property.
-          if (!doNotThrow && jsonResponse.result && jsonResponse.result.errors && jsonResponse.result.errors.length) {
-            // Reject errors with 409 Conflict status.
-            return Promise.reject(
-              handleError(
-                409,
-                {
-                  code: "DataError",
-                  message: jsonResponse.result.errors.join(", "),
-                },
-                source
-              )
-            );
-          } else {
-            // Either there are no errors or doNoThrow is set (in which case the callee must check `result.errors`).
-            // Do nothing, fall-through and return JSON response.
-          }
-          return jsonResponse;
+    if (response.ok) {
+      if (jsonResponse) {
+        // Successfully parsed JSON content.
+        // Check for data write errors. These differ from straight-forward invalid argument or validation
+        // failures. For example, a call to `updateData` might include requests to update 10 documents.
+        // If all 10 documents pass validation, the TDX will go ahead and attempt to write the data and
+        // will continue to apply updates even after one of them fails, i.e. the first 4 updates succeed,
+        // the fifth fails and the rest succeed. In this case `tdxResponse` will contain a `result` object with
+        // details of the failures in an `error` array property and the successes in an `commit` property.
+        if (!doNotThrow && jsonResponse.result && jsonResponse.result.errors && jsonResponse.result.errors.length) {
+          // Reject errors with 409 Conflict status.
+          return Promise.reject(
+            handleError(
+              409,
+              {
+                code: "DataError",
+                message: jsonResponse.result.errors.join(", "),
+              },
+              source
+            )
+          );
         } else {
-          // Response is OK but isn't in JSON format - this is usually for text content, e.g.new-line delimited JSON,
-          // markdown, html etc...
-          return text;
+          // Either there are no errors or doNoThrow is set (in which case the callee must check `result.errors`).
+          // Do nothing, fall-through and return JSON response.
         }
+        return jsonResponse;
       } else {
-        // Response has a non-200 status => see if the error is in JSON format.
-        if (jsonResponse && jsonResponse.error) {
-          // Build a failure object from the json response.
-          const failure = {code: jsonResponse.error, message: jsonResponse.error_description};
-          return Promise.reject(handleError(response.status, failure, source));
-        } else {
-          // The response body holds the error details.
-          return Promise.reject(handleError(response.status, jsonResponse || text, source));
-        }
+        // Response is OK but isn't in JSON format - this is usually for text content, e.g.new-line delimited JSON,
+        // markdown, html etc...
+        return text;
       }
-    });
+    } else {
+      // Response has a non-200 status => see if the error is in JSON format.
+      if (jsonResponse && jsonResponse.error) {
+        // Build a failure object from the json response.
+        const failure = {code: jsonResponse.error, message: jsonResponse.error_description};
+        return Promise.reject(handleError(response.status, failure, source));
+      } else {
+        // The response body holds the error details.
+        return Promise.reject(handleError(response.status, jsonResponse || text, source));
+      }
+    }
+  });
 };
 
-const setDefaults = function(config) {
+const setDefaults = function (config) {
   const log = debug("nqm-api-tdx:setDefaults");
 
   // Legacy config support.
@@ -383,16 +280,25 @@ const setDefaults = function(config) {
   config.queryServer = config.queryServer || config.queryHost;
 
   if (config.tdxServer && (!config.queryServer || !config.commandServer)) {
+    // If the query and command configurations are omitted attempt to derive them
+    // from the auth server (tdxServer) configuration.
     const protocolComponents = config.tdxServer.split("://");
     if (protocolComponents.length !== 2) {
       throw new Error(`invalid tdxServer in config - no protocol: ${config.tdxServer}`);
     }
     const protocol = protocolComponents[0];
     const hostComponents = protocolComponents[1].split(".");
-    if (hostComponents.length < 3) {
-      throw new Error(`invalid tdxServer in config - expected sub.domain.tld: ${config.tdxServer}`);
+
+    let hostname;
+    if (hostComponents.length === 2) {
+      // The auth server is not running under a subdomain (i.e. it is at the root).
+      hostname = hostComponents.join(".");
+    } else if (hostComponents.length === 3) {
+      // The auth server is running on a subdomain => strip it from the hostname.
+      hostname = hostComponents.slice(1).join(".");
+    } else {
+      throw new Error(`invalid tdxServer in config - expected 'domain.tld' or 'sub.domain.tld': ${config.tdxServer}`);
     }
-    const hostname = hostComponents.slice(1).join(".");
     config.databotServer = config.databotServer || `${protocol}://databot.${hostname}`;
     config.commandServer = config.commandServer || `${protocol}://cmd.${hostname}`;
     config.queryServer = config.queryServer || `${protocol}://q.${hostname}`;
@@ -406,14 +312,14 @@ const setDefaults = function(config) {
     config.commandServer || "[n/a]",
     config.databotServer || "[n/a]",
     config.queryServer || "[n/a]",
-    config.tdxServer || "[n/a]",
+    config.tdxServer || "[n/a]"
   );
 
   // Default network timeout to 2 mins.
   config.networkTimeout = config.networkTimeout === undefined ? 120000 : config.networkTimeout;
 };
 
-const waitForResource = function(resourceId, check, retryCount, maxRetries) {
+const waitForResource = function (resourceId, check, retryCount, maxRetries) {
   const log = debug("nqm-api-tdx:waitForResource");
   retryCount = retryCount || 0;
   return this.getResource(resourceId)
@@ -479,7 +385,7 @@ const waitForResource = function(resourceId, check, retryCount, maxRetries) {
     });
 };
 
-const waitForIndex = function(datasetId, status, maxRetries) {
+const waitForIndex = function (datasetId, status, maxRetries) {
   const log = debug("nqm-api-tdx:waitForIndex");
 
   // The argument maxRetries is optional.
@@ -491,7 +397,7 @@ const waitForIndex = function(datasetId, status, maxRetries) {
 
   let initialStatus = "";
 
-  const builtIndexCheck = function(dataset, retryCount) {
+  const builtIndexCheck = function (dataset, retryCount) {
     log("builtIndexCheck: %s", dataset ? dataset.indexStatus : "no dataset");
 
     let stopWaiting;
@@ -532,7 +438,7 @@ const waitForIndex = function(datasetId, status, maxRetries) {
   return waitForResource.call(this, datasetId, builtIndexCheck, 0, maxRetries);
 };
 
-const waitForAccount = function(accountId, verified, approved, retryCount, maxRetries) {
+const waitForAccount = function (accountId, verified, approved, retryCount, maxRetries) {
   const log = debug("nqm-api-tdx:waitForAccount");
   retryCount = retryCount || 0;
   return this.getAccount(accountId)
@@ -589,7 +495,6 @@ export {
   fetchWithDeadline,
   handleError,
   setDefaults,
-  TDXApiError,
   waitForAccount,
   waitForIndex,
 };
